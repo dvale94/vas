9129bdb9f42800ccdc418008a69a864ef782d451
Aurelien Meray <aurelien.meray@gmail.com>
2020-01-19 20:16:33 -0500

    created the Login component for SignIn

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,13 +1,100 @@
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import LockRoundedIcon from '@material-ui/icons/LockRounded';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { blueGrey, red, green, blue } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-class LoginForm extends Component {
-    render() {
-        return (
-            <div>
-                hello there
-            </div>
-        );
+const theme = createMuiTheme({
+    palette: {
+      primary: blue,
     }
-}
+  });
 
-export default LoginForm;
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: blueGrey[500],
+    '&:hover': {
+        backgroundColor: blue[500],
+    }
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: blueGrey[700],
+    color: "white",
+    fontWeight: "bold",
+    '&:hover': {
+        backgroundColor: blue[500],
+    }
+  },
+}));
+
+export default function LoginForm() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+        <LockRoundedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          VAS Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            color= "primary"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    </Container>
+    </ThemeProvider>
+  );
+}
