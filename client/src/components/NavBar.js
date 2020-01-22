7fa38513e95aeab4a9b816b1963bc3a28d7323e8
Aurelien Meray <aurelien.meray@gmail.com>
2020-01-21 20:35:29 -0500

    Customized the NavBar (styling + button actions)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,34 +1,78 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const classes = {
-    container: {
-        display: 'flex'
-    },
-    title: {
-        flexGrow: 1
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+          main: "#455a64"
+      },
     }
+  });
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    minHeight: 64,
+    alignItems: 'flex-start',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignSelf: 'flex-end',
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography className={classes.title} variant="h5" noWrap>
+           Volunteer Attendance System
+          </Typography>
+          <Button aria-label="display more actions" edge="end" color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+              Login
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+        >
+            <MenuItem onClick={handleClose}>Action 1</MenuItem>
+            <MenuItem onClick={handleClose}>Action 2</MenuItem>
+            <MenuItem onClick={handleClose}>Action 3</MenuItem>
+        </Menu>
+        </Toolbar>
+      </AppBar>
+    </div>
+    </ThemeProvider>
+  );
 }
 
-class NavBar extends Component {
-
-    render() {
-        return (
-            <div className={classes.container}>
-                <AppBar position='static'>
-                    <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
-}
-
-export default NavBar;
