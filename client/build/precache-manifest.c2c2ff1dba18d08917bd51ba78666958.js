1105d23e63ae669342991d9d41e5948e32c5fc1e
AurelienMeray <aurelien.meray@gmail.com>
2020-04-08 09:26:25 -0400

    updated favicon.ico for the broweser

---
+++ client/build/precache-manifest.c2c2ff1dba18d08917bd51ba78666958.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4919cc3d2ed27673894a691c595b3eea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c420e38a5aad600e794c",
+    "url": "/static/css/main.38ba69c2.chunk.css"
+  },
+  {
+    "revision": "06b9c2d85f1cc6cf25b1",
+    "url": "/static/js/2.9474d0dc.chunk.js"
+  },
+  {
+    "revision": "67a6b71a68fbc9c1ccd8eef05d8308ef",
+    "url": "/static/js/2.9474d0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c420e38a5aad600e794c",
+    "url": "/static/js/main.b46df0f8.chunk.js"
+  },
+  {
+    "revision": "b90b30feacb0b951743e",
+    "url": "/static/js/runtime-main.34553b58.js"
+  },
+  {
+    "revision": "133e973b62648100aa1594b545eca5d4",
+    "url": "/static/media/About.133e973b.png"
+  },
+  {
+    "revision": "75fd80251124af81f9bb30a32e3f9803",
+    "url": "/static/media/FIU_1_10.75fd8025.png"
+  },
+  {
+    "revision": "c5d90ffc4d7ab8e664e19e98c7b55f54",
+    "url": "/static/media/FIU_2_10.c5d90ffc.png"
+  },
+  {
+    "revision": "2d01488b64bdceda7b9041559b3f63d1",
+    "url": "/static/media/FIU_3_10.2d01488b.png"
+  },
+  {
+    "revision": "3ce02c699044e6128aa7ab762572ef73",
+    "url": "/static/media/FIU_4_10.3ce02c69.png"
+  },
+  {
+    "revision": "7dfd60709f5c82e830152cf0d86b86c7",
+    "url": "/static/media/FIU_7_10.7dfd6070.png"
+  },
+  {
+    "revision": "b9bcfeaaef65d40730e5095b29eadb91",
+    "url": "/static/media/FIU_8_10.b9bcfeaa.png"
+  },
+  {
+    "revision": "ed200c3b9aee94f7cea57a317e2321e1",
+    "url": "/static/media/FIU_9_10.ed200c3b.png"
+  },
+  {
+    "revision": "60950de861028c01d552eee39b35d3c9",
+    "url": "/static/media/VAS_shadow.60950de8.png"
+  },
+  {
+    "revision": "b57d7ba358d305df7593dd80b1ee5993",
+    "url": "/static/media/logo_shadow.b57d7ba3.png"
+  }
+]);
